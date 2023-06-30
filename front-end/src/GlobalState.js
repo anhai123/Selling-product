910989b4ef7c35ac2d9c8d24c2d88f4a04f4f293
lehuyhaianh0808@gmail.com <lehuyhaianh0808@gmail.com>
2023-06-30 10:36:03 +0700

    s

--- front-end/src/GlobalState.js
+++ front-end/src/GlobalState.js
@@ -15,7 +15,7 @@ export const DataProvider = ({ children }) => {
     var connectionOptions = {
       reconnectionAttempts: "Infinity", //avoid having user reconnect manually in order to prevent dead clients after a server restart
       timeout: 10000, //before connect_error and connect_timeout are emitted.
-
+      transports: ["websocket"],
       extraHeaders: {
         "Access-Control-Allow-Origin": "*",
       },
